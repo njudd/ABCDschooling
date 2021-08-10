01d75d0f1459726213f256eba52c5919acdcc009
nj_macbook <nickkjudd@gmail.com>
2021-08-10 18:35:41 +0200

    major edit, removed PCs since they are orthogonal

--- funcs/imp_3way.R
+++ funcs/imp_3way.R
@@ -15,43 +15,30 @@ imp_3way <- function(d){
   
   require(mice); require(miceadds)
   
-  
-d = cryst_data_pca[, c("site_id_l", "nihtbx_cryst_uncorrected.s", "ses_ppca.s", "pgs.s", "age_yrs", "schooling_yrs", "sex",
-  "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")]
-
-pred <- make.predictorMatrix(d)
-
-
-#colnames(d) <- c("site", "dv", "ses", "pgs", "age", "school", #"sex",
-#     "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")
-d$site_id_l <- as.numeric(as.factor(d$site_id_l))
-
-pred[,] <- 3
-pred[, "site_id_l"] <- -2
-pred[diag(dim(pred)[1])==1] <- 0
-
-# meth <- make.method(d)
-# meth[c("dv", "pgs", "ses", "age", "school", "sex",
-#        "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", 
-#        "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- "2l.pmm"
-
-imp <- mice(d, pred = pred, meth = "2l.pmm", seed = 919,
-            m = 10, print = FALSE)
-
-ct <- with(imp, lmer(nihtbx_cryst_uncorrected.s ~ 1 + age_yrs + schooling_yrs + sex + pgs.s + ses_ppca.s + (1 | site_id_l), REML = FALSE))
-summary(pool(ct))
+# Code when there is no interactions
+# d = cryst_data_pca[, c("site_id_l", "nihtbx_cryst_uncorrected.s", "ses_ppca.s", "pgs.s", "age_yrs", "schooling_yrs", "sex")]
+# 
+# pred <- make.predictorMatrix(d)
+# 
+# 
+# #colnames(d) <- c("site", "dv", "ses", "pgs", "age", "school", #"sex",
+# d$site_id_l <- as.numeric(as.factor(d$site_id_l))
+# 
+# pred[,] <- 3
+# pred[, "site_id_l"] <- -2
+# pred[diag(dim(pred)[1])==1] <- 0
+# 
+# imp <- mice(d, pred = pred, meth = "2l.pmm", seed = 919,
+#             m = 10, print = FALSE)
+# 
+# ct <- with(imp, lmer(nihtbx_cryst_uncorrected.s ~ 1 + age_yrs + schooling_yrs + sex + pgs.s + ses_ppca.s + (1 | site_id_l), REML = FALSE))
+# summary(pool(ct))
 
 
-
-
-  
-  colnames(d) <- c("site", "dv", "ses", "pgs", "age", "school", "sex",
-                   "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")
+  colnames(d) <- c("site", "dv", "ses", "pgs", "age", "school", "sex")
   d$site <- as.numeric(as.factor(d$site))
   
-  d <- data.frame(d, dv_m = NA, pgs_m = NA, ses_m = NA, age_m = NA, school_m = NA, #sex_m = NA, # holds the cluster means; you don't need sex since there is no interaction
-               #   C1_m = NA, C2_m = NA, C3_m = NA, C4_m = NA, C5_m = NA, C6_m = NA, C7_m = NA, C8_m = NA, C9_m = NA, C10_m = NA,  # they don't interact!
-              #    C11_m = NA, C12_m = NA, C13_m = NA, C14_m = NA, C15_m = NA, C16_m = NA, C17_m = NA, C18_m = NA, C19_m = NA, C20_m = NA, #20PCs
+  d <- data.frame(d, dv_m = NA, pgs_m = NA, ses_m = NA, age_m = NA, school_m = NA, sex_m = NA, # holds the cluster means; you don't need sex since there is no interaction
                   dv_d = NA, pgs_d = NA, ses_d = NA, school_d = NA, age_d = NA, # will hold the deviations from their cluster means
                   dv_d.ses_d = NA, dv_d.pgs_d = NA, dv_d.school_d = NA, dv_d.age_d = NA, # are two-way interactions of level-1 variables scaled as deviations from the cluster means
                   pgs_d.ses_d = NA, pgs_d.school_d = NA, ses_d.school_d = NA, age_d.pgs_d = NA, age_d.ses_d = NA,
@@ -59,104 +46,37 @@ summary(pool(ct))
   
   # level-1 variables
   meth <- make.method(d)
-  meth[c("dv", "pgs", "ses", "age", "school", "sex",
-         "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", 
-         "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- "2l.pmm"
-  
-  #?miceadds::mice.impute.2l.pmm()
+  meth[c("dv", "pgs", "ses", "age", "school", "sex")] <- "2l.pmm"
   
   pred <- make.predictorMatrix(d)
   pred[,] <- 0
   pred[, "site"] <- -2
   codes <- c(3, 3, 3, 3, 3 , 1, 1, 1, 1, 1)
   
-  pred["dv", c("pgs", "ses", "age", "school", "sex", "pgs_d.ses_d", "pgs_d.school_d", "ses_d.school_d", "pgs_d.ses_d.school_d", "age_d.pgs_d", "age_d.ses_d",
-               "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(codes, 1, rep(3, 20)) # 3 way added
+  pred["dv", c("pgs", "ses", "age", "school", "sex", "pgs_d.ses_d", "pgs_d.school_d", "ses_d.school_d", "pgs_d.ses_d.school_d", "age_d.pgs_d", "age_d.ses_d")] <- c(codes, 1) # 3 way added
   
-  pred["pgs", c("dv", "ses", "age", "school", "sex", "dv_d.ses_d", "dv_d.school_d", "dv_d.age_d", "ses_d.school_d", "age_d.ses_d", 
-                "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(codes, rep(3, 20))
-  pred["ses", c("dv", "pgs", "age", "school", "sex", "dv_d.pgs_d", "pgs_d.school_d", "dv_d.school_d", "dv_d.age_d", "age_d.pgs_d",
-                "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(codes, rep(3, 20))
+  pred["pgs", c("dv", "ses", "age", "school", "sex", "dv_d.ses_d", "dv_d.school_d", "dv_d.age_d", "ses_d.school_d", "age_d.ses_d")] <- c(codes)
+  pred["ses", c("dv", "pgs", "age", "school", "sex", "dv_d.pgs_d", "pgs_d.school_d", "dv_d.school_d", "dv_d.age_d", "age_d.pgs_d")] <- c(codes)
   
-  pred["school", c("dv", "pgs", "ses", "age", "sex", "dv_d.pgs_d", "dv_d.ses_d", "pgs_d.ses_d", "age_d.pgs_d", "age_d.ses_d", "dv_d.age_d", # one more because there is no school*age interaction
-                   "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(codes, 1, rep(3, 20))
+  pred["school", c("dv", "pgs", "ses", "age", "sex", "dv_d.pgs_d", "dv_d.ses_d", "pgs_d.ses_d", "age_d.pgs_d", "age_d.ses_d", "dv_d.age_d")] <- c(codes, 1) # one more because there is no school*age inter
   
-  # beacuse of the 3 way there's one less
-  pred["age", c("dv", "pgs", "ses", "school", "sex", "dv_d.pgs_d", "dv_d.ses_d", "pgs_d.ses_d", "pgs_d.school_d", "dv_d.school_d", "ses_d.school_d", "pgs_d.ses_d.school_d",
-                "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(codes,1,1, rep(3, 20))
+  pred["age", c("dv", "pgs", "ses", "school", "sex", "dv_d.pgs_d", "dv_d.ses_d", "pgs_d.ses_d", "pgs_d.school_d", "dv_d.school_d", "ses_d.school_d", "pgs_d.ses_d.school_d")] <- c(codes,1,1)  # because of the 3 way there's one less
   
   
   
   
   # not included in interactions...
-  pred["sex", c("dv", "pgs", "ses", "school", "age",
-                "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  
-  # making the others predict off the PCs
-  
-  pred["C1", c("dv", "pgs", "ses", "school", "age", "sex",
-                "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C2", c("dv", "pgs", "ses", "school", "age", "sex",
-               "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C3", c("dv", "pgs", "ses", "school", "age", "sex",
-               "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C4", c("dv", "pgs", "ses", "school", "age", "sex",
-               "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C5", c("dv", "pgs", "ses", "school", "age", "sex",
-               "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  
-  pred["C6", c("dv", "pgs", "ses", "school", "age", "sex",
-               "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C7", c("dv", "pgs", "ses", "school", "age", "sex",
-               "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C8", c("dv", "pgs", "ses", "school", "age", "sex",
-               "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C9", c("dv", "pgs", "ses", "school", "age", "sex",
-               "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C10", c("dv", "pgs", "ses", "school", "age", "sex",
-               "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C1", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  
-  pred["C11", c("dv", "pgs", "ses", "school", "age", "sex",
-                "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C12", c("dv", "pgs", "ses", "school", "age", "sex",
-                "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C13", c("dv", "pgs", "ses", "school", "age", "sex",
-                "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C14", c("dv", "pgs", "ses", "school", "age", "sex",
-                "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C15", c("dv", "pgs", "ses", "school", "age", "sex",
-               "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C1", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  
-  pred["C16", c("dv", "pgs", "ses", "school", "age", "sex",
-                "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C17", c("dv", "pgs", "ses", "school", "age", "sex",
-                "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C18", c("dv", "pgs", "ses", "school", "age", "sex",
-                "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C19", c("dv", "pgs", "ses", "school", "age", "sex",
-                "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  pred["C20", c("dv", "pgs", "ses", "school", "age", "sex",
-                "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- c(rep(3, 25))
-  
+  pred["sex", c("dv", "pgs", "ses", "school", "age")] <- c(rep(3, 5))
   
   
   # starting to do things for the interactions, we need to get the means so we can subtract them so we can get there deviances, which than we take the product of
   
   # derive group means
   meth[c("pgs_m", "ses_m", "dv_m", "age_m", "school_m")] <- "2l.groupmean"
-         # "C1_m", "C2_m", "C3_m", "C4_m", "C5_m", "C6_m", "C7_m", "C8_m", "C9_m", "C10_m", 
-         # "C11_m", "C12_m", "C13_m", "C14_m", "C15_m", "C16_m", "C17_m", "C18_m", "C19_m", "C20_m")] <- "2l.groupmean"
   
-  pred[c("pgs_m", "ses_m", "dv_m", "age_m", "school_m"), 
-         # "sex_m", "C1_m", "C2_m", "C3_m", "C4_m", "C5_m", "C6_m", "C7_m", "C8_m", "C9_m", "C10_m", 
-         # "C11_m", "C12_m", "C13_m", "C14_m", "C15_m", "C16_m", "C17_m", "C18_m", "C19_m", "C20_m"), 
+  pred[c("pgs_m", "ses_m", "dv_m", "age_m", "school_m"),
        c("pgs", "ses", "dv", "age", "school")] <- diag(5)
-         # "sex", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", 
-         # "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")] <- diag(26)
-         # 
-  
-  
-  
+
   # derive deviations from cluster mean, within each cluster the deviation from the grad mean is coded
   meth["pgs_d"] <- "~ I(pgs - pgs_m)"
   meth["dv_d"] <- "~ I(dv - dv_m)"
@@ -182,13 +102,12 @@ summary(pool(ct))
   meth["pgs_d.ses_d.school_d"] <- "~ I(ses_d * school_d * pgs_d)"
   
   
-  visit <- c("dv", "dv_m", "dv_d", "dv_d.ses_d", "dv_d.pgs_d", "dv_d.school_d",
+  visit <- c("dv", "dv_m", "dv_d", "dv_d.ses_d", "dv_d.pgs_d", "dv_d.school_d", "dv_d.age_d",
              "pgs", "pgs_m", "pgs_d", "pgs_d.ses_d", "dv_d.pgs_d", "pgs_d.school_d", "pgs_d.ses_d.school_d",
              "ses", "ses_m", "ses_d", "pgs_d.ses_d", "dv_d.ses_d", "ses_d.school_d", "pgs_d.ses_d.school_d",
              "age", "age_m", "age_d", "age_d.pgs_d", "age_d.ses_d",
              "school", "school_m", "school_d", "dv_d.school_d", "pgs_d.school_d", "ses_d.school_d", "pgs_d.ses_d.school_d",
-             "sex", 
-             "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20")
+             "sex")
   
   imp <- mice(d, pred = pred, meth = meth, seed = 188,
               visit = visit, m = 20, print = FALSE,
